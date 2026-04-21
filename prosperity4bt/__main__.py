ab31f63f786127e1cb4cb6d101191134f97a46ba
alex <75824220+alexolcott@users.noreply.github.com>
2026-04-21 19:58:52 -0400

    changes for prosperity 4

--- prosperity4bt/__main__.py
+++ prosperity4bt/__main__.py
@@ -8,11 +8,11 @@ from typing import Annotated, Any, Optional
 
 from typer import Argument, Option, Typer
 
-from prosperity3bt.data import has_day_data
-from prosperity3bt.file_reader import FileReader, FileSystemReader, PackageResourcesReader
-from prosperity3bt.models import BacktestResult, TradeMatchingMode
-from prosperity3bt.open import open_visualizer
-from prosperity3bt.runner import run_backtest
+from prosperity4bt.data import has_day_data
+from prosperity4bt.file_reader import FileReader, FileSystemReader, PackageResourcesReader
+from prosperity4bt.models import BacktestResult, TradeMatchingMode
+from prosperity4bt.open import open_visualizer
+from prosperity4bt.runner import run_backtest
 
 
 def parse_algorithm(algorithm: Path) -> Any:
@@ -173,7 +173,7 @@ def format_path(path: Path) -> str:
 
 def version_callback(value: bool) -> None:
     if value:
-        print(f"prosperity3bt {metadata.version(__package__)}")
+        print(f"prosperity4bt {metadata.version(__package__)}")
         sys.exit(0)
 
 
@@ -185,10 +185,10 @@ def cli(
     algorithm: Annotated[Path, Argument(help="Path to the Python file containing the algorithm to backtest.", show_default=False, exists=True, file_okay=True, dir_okay=False, resolve_path=True)],
     days: Annotated[list[str], Argument(help="The days to backtest on. <round>-<day> for a single day, <round> for all days in a round.", show_default=False)],
     merge_pnl: Annotated[bool, Option("--merge-pnl", help="Merge profit and loss across days.")] = False,
-    vis: Annotated[bool, Option("--vis", help="Open backtest results in https://jmerle.github.io/imc-prosperity-3-visualizer/ when done.")] = False,
+    vis: Annotated[bool, Option("--vis", help="Open backtest results in https://alexolcott.github.io/imc-prosperity-4-visualizer/ when done.")] = False,
     out: Annotated[Optional[Path], Option(help="File to save output log to (defaults to backtests/<timestamp>.log).", show_default=False, dir_okay=False, resolve_path=True)] = None,
     no_out: Annotated[bool, Option("--no-out", help="Skip saving output log.")] = False,
-    data: Annotated[Optional[Path], Option(help="Path to data directory. Must look similar in structure to https://github.com/jmerle/imc-prosperity-3-backtester/tree/master/prosperity3bt/resources.", show_default=False, exists=True, file_okay=False, dir_okay=True, resolve_path=True)] = None,
+    data: Annotated[Optional[Path], Option(help="Path to data directory. Must look similar in structure to https://github.com/alexolcott/imc-prosperity-4-backtester/tree/master/prosperity4bt/resources.", show_default=False, exists=True, file_okay=False, dir_okay=True, resolve_path=True)] = None,
     print_output: Annotated[bool, Option("--print", help="Print the trader's output to stdout while it's running.")] = False,
     match_trades: Annotated[TradeMatchingMode, Option(help="How to match orders against market trades. 'all' matches trades with prices equal to or worse than your quotes, 'worse' matches trades with prices worse than your quotes, 'none' does not match trades against orders at all.")] = TradeMatchingMode.all,
     no_progress: Annotated[bool, Option("--no-progress", help="Don't show progress bars.")] = False,
